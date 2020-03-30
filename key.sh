f710e9cbf0167209bccdc6d0870a0c885b297078
Amit Dalal <sameerashle@gmail.com>
2020-03-30 15:20:53 +0530

    Update key.sh

--- key.sh
+++ key.sh
@@ -4,22 +4,18 @@ keytime=`date +%Y%m%d%H%M`
 
 if [[ -d ~/.ssh ]] 
   then
- ssh-keygen -f /tmp/key-$keytime -N ''
- 
-cat /tmp/key-$keytime.pub >> ~/.ssh/authorized_keys
-
-echo "Please find the private key below"
-
-cat /tmp/key-$keytime
-rm -rf /tmp/key-$keytime 
+   ssh-keygen -f /tmp/key-$keytime -N ''
+   cat /tmp/key-$keytime.pub >> ~/.ssh/authorized_keys
+   chmod 400 ~/.ssh/authorized_keys
+   echo "Please find the private key below"
+   cat /tmp/key-$keytime
+   rm -rf /tmp/key-$keytime 
 else 
   mkdir ~/.ssh && chmod 700 ~/.ssh
   ssh-keygen -f /tmp/key-$keytime -N ''
- 
   cat /tmp/key-$keytime.pub >> ~/.ssh/authorized_keys
-
+  chmod 400 ~/.ssh/authorized_keys
   echo "Please find the private key below"
-
   cat /tmp/key-$keytime
   rm -rf /tmp/key-$keytime
 fi
